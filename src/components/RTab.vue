fbe0d6f15ee909985f3dfdd45007fd9c3aa3f258
Reiner New <nbxlhc@hotmail.com>
2025-05-13 19:16:18 +0800

    add component RTab

---
+++ src/components/RTab.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="tab-wrapper" style="position: relative;">
+    <q-tab :name="props.name" :label="props.label" :icon="props.icon" :selected="props.selected" />
+    <q-btn
+      flat
+      round
+      dense
+      size="xs"
+      icon="close"
+      style="position: absolute; top: 0; left: 0;"
+      @click="onClick"
+    />
+  </div>
+</template>
+
+
+<script setup>
+defineOptions({
+  name: '6t',
+})
+
+const props = defineProps({
+  name: {
+    type: String,
+    default: ''
+  },
+
+  label: {
+    type: String,
+    default: ''
+  },
+
+  icon: {
+    type: String,
+    default: ''
+  },
+
+  selected: {
+    type: Function,
+    default: () => {}
+  },
+
+  onClick: {
+    type: Function,
+    default: () => {}
+  }
+})
+
+const onClick = (e) => {
+  e.stopPropagation()
+
+  props.onClick()
+}
+
+
+</script>
+
+<style scoped>
+.tab-wrapper {
+  display: inline-block; /* 确保包装器紧贴 q-tab 的尺寸 */
+}
+
+</style>
